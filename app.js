2ee34532ccda2cf332bec299a526c4805d8d3169
CesarBerta <cesar_fb@hotmail.com>
2024-02-22 19:04:10 -0300

    Estructura MVC

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+
+const path = require ("path");
+
+// ruta absoluta carpeta public
+const publicPath = path.resolve(__dirname , "./public");
+
+app.use (express.static (publicPath));
+
+/* inicializo servidor 3043 ruta localhost:3043 */
+app.listen (3043, () => 
+    console.log("Servidor corriendo en Puerto 3043")
+);
+
+/* ruta home localhost:3043 */
+app.get("/", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/index.html"))
+});
+
+/* ruta register localhost:3043/register */
+app.get("/register", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+});
+
+/* ruta login localhost:3043/login */
+app.get("/login", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+});
+
+/* ruta Detalle Producto localhost:3043/productDetail */
+app.get("/productDetail", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/productDetail.html"))
+});
+
+/* ruta Carrito de compras localhost:3043/productCart */
+app.get("/productCart", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/productCart.html"))
+});
---
+++ retro.md
@@ -0,0 +1,10 @@
+# Retrospectiva Sprint 1
+
+## Dinamica Estrella de Mar
+
+1. Comenzar a hacer.
+2. Hacer más.
+3. Continuar haciendo.
+4. Hacer menos.
+5. Dejar de hacer.
+
