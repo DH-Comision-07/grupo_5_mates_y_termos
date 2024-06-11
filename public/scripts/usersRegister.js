37b2ee181f5aa07b5349a5415d84dea4333c09e2
facukomorovski <facukomorovski95@gmail.com>
2024-06-11 11:49:29 -0300

    name and lastName validations

--- public/scripts/usersRegister.js
+++ public/scripts/usersRegister.js
@@ -3,29 +3,31 @@ window.addEventListener("load", function(){
 
     formulario.addEventListener("submit", function(e){
         let errores = [];
-        let campoNombre = document.querySelector("input.main__form--inputs");
+        let campoNombre = document.querySelector("input.name");
 
         if(campoNombre.value == ""){
             errores = [];
             errores.push("El campo Nombre tiene que estar completo");
-        } else if(campoNombre.value.length < 3){
-            errores.push("El campo Nombre tiene que tener al menos tres caracteres");
+        } else if(campoNombre.value.length < 2){
+            errores.push("El campo Nombre tiene que tener al menos dos caracteres");
         }
         if (errores.length > 0){
             e.preventDefault();
-            let ulErrores = document.querySelector("div.erroresName ul");
+            let ulErrores = document.querySelector("div.erroresName");
             for (let i = 0; i < errores.length; i++) {
             ulErrores.innerHTML += "<li>"+ errores[i] + "</li>" } 
         }
         let campoApellido = document.querySelector("input.lastName");
-
+        
         if(campoApellido.value == ""){
             errores = [];
             errores.push("El campo Apellido tiene que estar completo");
+        }else if(campoApellido.value.length < 2){
+            errores.push("El campo Apellido tiene que tener al menos dos caracteres");
         }
         if (errores.length > 0){
             e.preventDefault();
-            let ulErrores = document.querySelector("div.erroresLastName ul");
+            let ulErrores = document.querySelector("div.erroresLastName");
             for (let i = 0; i < errores.length; i++) {
             ulErrores.innerHTML += "<li>"+ errores[i] + "</li>" } 
     }})
